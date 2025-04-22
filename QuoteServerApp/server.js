fef3deb490de5e862326204c3cf953e00131201f
MadisonSmeallie <mm06smea@siena.edu>
2025-04-21 22:48:33 -0400

    Added quote data structures to the server.js file

---
+++ QuoteServerApp/server.js
@@ -0,0 +1,37 @@
+"use strict"
+
+let categories = ['successQuotes', 'perseveranceQuotes', 'happinessQuotes'];
+
+let successQuotes = [
+  {
+    quote: 'Success is not final, failure is not fatal: It is the courage to continue that counts.',
+    author: 'Winston S. Churchill'
+  },
+  {
+    quote: 'The way to get started is to quit talking and begin doing.',
+    author: 'Walt Disney'
+  }
+];
+
+let perseveranceQuotes = [
+  {
+    quote: 'It’s not that I’m so smart, it’s just that I stay with problems longer.',
+    author: 'Albert Einstein'
+  },
+  {
+    quote: 'Perseverance is failing 19 times and succeeding the 20th.',
+    author: 'Julie Andrews'
+  }
+];
+
+let happinessQuotes = [
+  {
+    quote: 'Happiness is not something ready made. It comes from your own actions.',
+    author: 'Dalai Lama'
+  },
+  {
+    quote: 'For every minute you are angry you lose sixty seconds of happiness.',
+    author: 'Ralph Waldo Emerson'
+  }
+];
+
